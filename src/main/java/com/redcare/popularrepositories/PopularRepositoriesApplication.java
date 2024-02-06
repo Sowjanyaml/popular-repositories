b64029b09d3549e5a3521104d4dd9cfc9ca695fe
lakshmisowjanya <sowjanya.ml@gmail.com>
2024-02-06 11:28:06 +0100

    Basic setup

---
+++ src/main/java/com/redcare/popularrepositories/PopularRepositoriesApplication.java
@@ -0,0 +1,13 @@
+package com.redcare.popularrepositories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopularRepositoriesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PopularRepositoriesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/redcare/popularrepositories/PopularRepositoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redcare.popularrepositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopularRepositoriesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
