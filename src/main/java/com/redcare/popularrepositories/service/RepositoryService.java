6cf1e85bbfd3992f185c43f9085977d1d93a6f41
lakshmisowjanya <sowjanya.ml@gmail.com>
2024-02-08 16:49:26 +0100

    ♻️ refactor

--- src/main/java/com/redcare/popularrepositories/service/RepositoryService.java
+++ src/main/java/com/redcare/popularrepositories/service/RepositoryService.java
@@ -1,10 +1,13 @@
 package com.redcare.popularrepositories.service;
 
 import com.redcare.popularrepositories.client.GitHubClient;
+import com.redcare.popularrepositories.exception.InvalidLimitPerPage;
 import com.redcare.popularrepositories.model.github.Item;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
+import static com.redcare.popularrepositories.util.DateValidator.dateValidation;
 
 @Service
 public class RepositoryService {
@@ -16,7 +19,12 @@ public class RepositoryService {
     }
 
     public List<Item> getPopularRepositories(int limit, String fromDate, String language) {
+        if (limit <= 0) {
+            throw new InvalidLimitPerPage("The value " + limit + " is not a valid page limit. Please enter a valid limit per page.");
+        }
+        dateValidation(fromDate);
         String query = "created:" + fromDate + (language != null ? " language:" + language : "");
         return gitHubClient.searchRepositories(query, "stars", "desc", limit);
     }
+
 }
